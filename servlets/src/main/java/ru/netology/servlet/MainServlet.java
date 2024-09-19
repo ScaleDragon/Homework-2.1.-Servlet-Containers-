c551894277acedb38795efdd9a48194cdd7ae920
Vyacheslav Lukin <dwek18@mail.ru>
2024-09-19 20:21:26 +0300

    add di-java

---
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,62 @@
+package ru.netology.servlet;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.JavaConfig.JavaConfig;
+import ru.netology.controller.PostController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+
+    private static final String API_POSTS = "/api/posts";
+    private static final String API_POSTS_ID_PATTERN = "/api/posts/\\d+";
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
+
+    @Override
+    public void init() {
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        controller = context.getBean(PostController.class);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(GET) && path.equals(API_POSTS)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(GET) && path.matches(API_POSTS_ID_PATTERN)) {
+                // easy way
+                controller.getById(getPostID(path), resp);
+                return;
+            }
+            if (method.equals(POST) && path.equals(API_POSTS)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE) && path.matches(API_POSTS_ID_PATTERN)) {
+                // easy way
+                controller.removeById(getPostID(path), resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private long getPostID(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/")));
+    }
+}
+
